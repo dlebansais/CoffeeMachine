53d8571bc08b7404b8051a973aeb9623dec120b6
dlebansais <dlebansais@gmail.com>
2022-10-13 21:45:02 +0200

    Added a demo project and a readme.

---
+++ Demo/Demo-CoffeeMachine/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Globalization;
+using System;
+using CoffeeMachine;
+
+internal class Program
+{
+    internal static void Main(string[] args)
+    {
+        // Initializing the coffee machine.
+        ICoffeeMachine CoffeeMachine = BasicCoffeeMachine.Create();
+
+        // Display available drinks.
+        Console.WriteLine("Veuillez choisir une boisson :");
+
+        for (int Index = 0; Index < CoffeeMachine.DrinkList.Count; Index++)
+        {
+            SelectableDrink Item = CoffeeMachine.DrinkList[Index];
+            IRecipe Recipe = Item.Recipe;
+            string RecipeName = Recipe.Name;
+            int SelectionIndex = Index + 1;
+            string Line = $"{SelectionIndex} : {RecipeName}";
+
+            Console.WriteLine(Line);
+        }
+
+        Console.WriteLine("Pour quitter, appuyez sur Ctrl+C.");
+
+        // Perform selection until Ctrl+C.
+        for (; ; )
+        {
+            // Read the next key from the console.
+            ConsoleKeyInfo KeyInfo = Console.ReadKey();
+            char KeyChar = KeyInfo.KeyChar;
+            Console.WriteLine();
+
+            // Translate the key to a selection.
+            if (KeyChar >= '1' && KeyChar < '1' + CoffeeMachine.DrinkList.Count)
+            {
+                int SelectedIndex = KeyChar - '1';
+                SelectableDrink SelectedDrink = CoffeeMachine.DrinkList[SelectedIndex];
+                IRecipe selectedRecipe = CoffeeMachine.DrinkList[SelectedIndex].Recipe;
+                double SalePrice = Math.Round(SelectedDrink.Price, 2);
+                string PriceAsString = SalePrice.ToString(CultureInfo.InvariantCulture);
+
+                string Line = $"Ce {selectedRecipe.Name} coûte {PriceAsString} euros.";
+
+                Console.WriteLine(Line);
+            }
+            else
+                Console.WriteLine("Désolé, cette sélection n'est pas disponible.");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+
+[![Build status](https://ci.appveyor.com/api/projects/status/ciok0j05p14mgx3w?svg=true)](https://ci.appveyor.com/project/dlebansais/coffeemachine) 
+[![codecov](https://codecov.io/gh/dlebansais/CoffeeMachine/branch/master/graph/badge.svg?token=ZKVoed9Z0F)](https://codecov.io/gh/dlebansais/CoffeeMachine)
+
+# CoffeeMachine
+A sample program for demo purpose.
+
+# Details
+Run Demo-CoffeeMachine as console (Select and set as startup program, then F5 in Visual Studio).
+ 
