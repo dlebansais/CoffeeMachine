2cb031d7853a43f93113ac32d8b7ecd38c11af5e
dlebansais <dlebansais@gmail.com>
2022-10-13 18:47:49 +0200

    Dose: new tests.

--- Test/Test-CoffeeMachine/TestDose/TestDose.Constructor.cs
+++ Test/Test-CoffeeMachine/TestDose/TestDose.Constructor.cs
@@ -2,6 +2,7 @@
 
 using CoffeeMachine;
 using NUnit.Framework;
+using System;
 
 [TestFixture]
 internal partial class TestDose
@@ -36,5 +37,28 @@ internal partial class TestDose
 
         Assert.That(NewInstance.Ingredient, Is.EqualTo(TestIngredient));
         Assert.That(NewInstance.Quantity, Is.EqualTo(TestQuantity));
+    }
+
+    [Test]
+    public void TestInvalidIngredient()
+    {
+        IIngredient TestIngredient = null!;
+        int TestQuantity = 1;
+
+        Assert.Throws<ArgumentNullException>(() => new Dose(TestIngredient));
+        Assert.Throws<ArgumentNullException>(() => new Dose(TestIngredient, TestQuantity));
+    }
+
+    [Test]
+    public void TestInvalidQuantity()
+    {
+        IIngredient TestIngredient = new BasicIngredient("No name", 0);
+        int TestQuantityInvalid1 = 0;
+        int TestQuantityInvalid2 = -1;
+        int TestQuantityInvalid3 = int.MinValue;
+
+        Assert.Throws<ArgumentException>(() => new Dose(TestIngredient, TestQuantityInvalid1));
+        Assert.Throws<ArgumentException>(() => new Dose(TestIngredient, TestQuantityInvalid2));
+        Assert.Throws<ArgumentException>(() => new Dose(TestIngredient, TestQuantityInvalid3));
     }
 }
