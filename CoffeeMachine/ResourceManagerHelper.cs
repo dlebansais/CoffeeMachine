4c77ff2e83ec14ee6df168033afc3d562875ba45
dlebansais <dlebansais@gmail.com>
2022-10-13 21:34:07 +0200

    Added tests for localized strings.

---
+++ CoffeeMachine/ResourceManagerHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace CoffeeMachine;
+
+using System.Resources;
+
+/// <summary>
+/// Provides methods to read localized resources.
+/// </summary>
+public static class ResourceManagerHelper
+{
+    /// <summary>
+    /// Reads a localized string in resources.
+    /// </summary>
+    /// <param name="id">The string identifier.</param>
+    /// <param name="cultureInvariantString">The culture-invariant string to return if not found.</param>
+    /// <returns>The localized string.</returns>
+    public static string ReadString(string id, string cultureInvariantString)
+    {
+        return cultureInvariantString;
+    }
+}
---
+++ Test/Test-CoffeeMachine/TestResourceManager/TestResourceManager.cs
@@ -0,0 +1,26 @@
+﻿namespace TestCoffeeMachine;
+
+using CoffeeMachine;
+using NUnit.Framework;
+
+[TestFixture]
+internal partial class TestResourceManager
+{
+    [Test]
+    public void TestResourceFound()
+    {
+        string TestString = "Should not be used";
+        string ReadResult = ResourceManagerHelper.ReadString(nameof(WellKnownIngredient.Coffee), TestString);
+
+        Assert.That(ReadResult, Is.Not.EqualTo(TestString));
+    }
+
+    [Test]
+    public void TestResourceNotFound()
+    {
+        string TestString = "Should be used";
+        string ReadResult = ResourceManagerHelper.ReadString(string.Empty, TestString);
+
+        Assert.That(ReadResult, Is.EqualTo(TestString));
+    }
+}
